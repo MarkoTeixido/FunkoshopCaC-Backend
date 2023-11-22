1e925bf06303fef92026f49760f0dc47012ce886
MarkoTeixido <teixido_marko@outlook.es>
2023-11-22 17:35:57 -0300

    [FEAT] - template engine EJS configurados

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
+
+//Import routes
 const mainRoutes = require('./src/router/mainRoutes');
+//
 
+//Set static file folder
 app.use(express.static('public'));
+//
 
+//Set Template Engine (EJS)
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+//
+
+//Set plication routes
 app.use('/', mainRoutes);
+//
 
-app.listen(4000, () => console.log("servidor de Funkoshop corriendo en http://localhost:4000"));
+//Set the server to be heard on port 4000
+app.listen(4000, () => console.log("🚀 servidor de Funkoshop corriendo en http://localhost:4000"));
