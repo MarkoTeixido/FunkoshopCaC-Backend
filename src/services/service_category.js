27e4aa9efb84937bd1bef8de8d8a5901a856bd81
MarkoTeixido <teixido_marko@outlook.es>
2023-12-12 14:48:12 -0300

    [FEAT] - service_category terminado

---
+++ src/services/service_category.js
@@ -0,0 +1,67 @@
+const { category } = require('../models/model_category');
+
+// Service licence (GET) get all
+const getAllCategory = async () => {
+    try {
+      const rows = await category.findAll();
+  
+      const response = {
+        isError: false,
+        data: rows,
+      };
+
+      console.log(JSON.stringify(response, null, 2));
+  
+      return response;
+    } catch (e) {
+      const error = {
+        isError: true,
+        message: `No pudimos recuperar los datos ${e}.`,
+      };
+  
+      console.error(JSON.stringify(error, null, 2)); 
+  
+      return error;
+    }
+};
+
+//getAllCategory();
+
+
+// Service licence (GET) get one by id
+const getCategoryById = async (categoryId) => {
+    try {
+      const row = await category.findByPk(categoryId);
+  
+      if (!row) {
+        const error = {
+          isError: true,
+          message: `No se encontró ningún producto con el ID ${productId}.`,
+        };
+  
+        console.error(JSON.stringify(error, null, 2));
+  
+        return error;
+      }
+  
+      const response = {
+        isError: false,
+        data: row,
+      };
+  
+      console.log(JSON.stringify(response, null, 2));
+  
+      return response;
+    } catch (e) {
+      const error = {
+        isError: true,
+        message: `No pudimos recuperar los datos ${e}.`,
+      };
+  
+      console.error(JSON.stringify(error, null, 2));
+  
+      return error;
+    }
+};
+
+//getCategoryById(1);
