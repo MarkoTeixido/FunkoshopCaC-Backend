c70190c45b042eb00feda331f73aa72323c337b5
TomasRivetta <totorivetta@gmail.com>
2023-12-13 21:13:10 -0300

    modificando el updateView

--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -2,22 +2,24 @@ const dataProduct = require("../services/service_product");
 const datalicence = require("../services/service_licence");
 const dataCategory = require("../services/service_category");
 
+const model = require("../models/model_product")
+
 const adminControllers = {
     index: async (req, res) => {
-        try{
+        try {
             const collections = await dataProduct.getAllProducts();
             res.render('admin/admin', {
                 view: {
                     title: "Admin | Productos"
                 },
                 collections: collections.data,
-            }); 
-        } catch(error){
+            });
+        } catch (error) {
             console.log(error);
         }
     },
     createView: async (req, res) => {
-        try{
+        try {
             const licences = await datalicence.getAllLicence();
             const categories = await dataCategory.getAllCategory();
             res.render('admin/create', {
@@ -27,35 +29,53 @@ const adminControllers = {
                 licences: licences.data,
                 categories: categories.data,
             });
-        } catch(error){
+        } catch (error) {
             console.log(error);
-        }    
+        }
     },
     create: async (req, res) => {
-        try{
+        try {
             const item = req.body;
             await dataProduct.createProduct(item);
             res.redirect('/admin/products');
-        } catch(error){
+        } catch (error) {
             console.log(error);
         }
     },
     updateView: async (req, res) => {
-        try{
+        try {
             const licences = await datalicence.getAllLicence();
             const categories = await dataCategory.getAllCategory();
+            const product = await dataProduct.getProductById(req.params.id)
             res.render('admin/edit', {
                 view: {
                     title: "Admin | Editar producto"
                 },
                 licences: licences.data,
                 categories: categories.data,
+                product: product.data,
             });
-        } catch(error){
+        } catch (error) {
             console.log(error);
         }
     },
-    update: (req, res) => res.send('Route for edit a product'),
+    update: async (req, res) => {
+
+        try {
+            const affected = await dataProduct.updateProductById(req.params.id, req.body);
+            console.log(affected);
+            console.log(`Te devuelvo esto: ${affected[0]} HASTA ACAAAAA`);
+            if (affected[0] == 1) {
+                res.redirect("/admin/productos");
+            } else {
+                res.status(500).send("Error al actualizar el producto");
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(500).send(error);
+        }
+
+    },
     delete: async (req, res) => {
         try {
             await dataProduct.deleteProductById(req.params.id);
