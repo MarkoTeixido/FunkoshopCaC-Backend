f9ab93ed9cec86981c3dbe31d9977df08d1beff3
MarkoTeixido <teixido_marko@outlook.es>
2023-12-15 18:02:32 -0300

    [FEAT] - probando post register

---
+++ src/utils/user_validations.js
@@ -0,0 +1,62 @@
+const { body } = require("express-validator");
+const userModel = require("../models/model_user");
+
+const registerValidations = [
+    body("email")
+      .isEmail()
+      .withMessage("Ingrese una dirección de correo electrónico válida")
+      .bail()
+      .custom((value, { req }) => {
+        return new Promise(async (resolve, reject) => {
+          try {
+            const user = await userModel.findOne({
+              where: {
+                email: value,
+              },
+            });
+  
+            if (user) {
+              console.log(user);
+              return reject();
+            } else {
+              return resolve();
+            }
+          } catch (error) {
+            console.log(error);
+          }
+        });
+      })
+      .withMessage("Dirección de correo electrónico duplicada"),
+    body("password")
+      .isStrongPassword({
+        minLength: 6,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      })
+      .withMessage("La contraseña debe tener ...")
+      .bail()
+      .custom((value, { req }) => value === req.body.password_confirmation)
+      .withMessage("Las contraseñas no coinciden"),
+];
+  
+const loginValidations = [
+    body("email")
+      .isEmail()
+      .withMessage("Ingrese una dirección de correo electrónico válida"),
+    body("password")
+      .isStrongPassword({
+        minLength: 6,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      })
+      .withMessage("La contraseña debe tener ..."),
+];
+
+module.exports = { 
+    registerValidations,
+    loginValidations,
+};
